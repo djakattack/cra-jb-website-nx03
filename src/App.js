d2ebdc9c67d5b04c2540021813e93de6bdb30651
jpearce2014 <jlpearce.phd@gmail.com>
2022-07-08 13:26:14 -0400

    replaced generic CRA atom and text with text for the restaurant and branding iron logo.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
+import logo from './assetts/images/logo-white.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src={logo} alt="logo"/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Johnny Bull's Steakhouse
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          <i>a great meal is around the corner!</i>
+        </p>
       </header>
     </div>
   );
